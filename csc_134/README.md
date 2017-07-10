8b8d6eced0ace50a9606119ec419ae7dcb652495
Nicholas Wade <nwade514@gmail.com>
2017-07-10 15:18:13 -0700

    added original assignments for csc_134-assignment_1

---
+++ csc_134/README.md
@@ -0,0 +1,2 @@
+# csc_134
+Couse work for CSC-134 (C++ Programming), Spring 2011 semester.
---
+++ csc_134/assignment_1/lab-2_rectangle-area_original.cpp
@@ -0,0 +1,17 @@
+// This program will output the circumference and area
+// of the circle with a given radius.
+// PLACE YOUR NAME HERE
+#include <iostream>
+using namespace std;
+int length = 8;
+int width = 3;
+int main()
+{
+int area;
+int perimeter;
+area = length*width;
+perimeter= (2*length)+(2*width);
+cout<< "The area of the rectangle is "<<area<< endl;
+cout<< "The perimeter of the rectangle is " <<perimeter;
+return 0;
+}
